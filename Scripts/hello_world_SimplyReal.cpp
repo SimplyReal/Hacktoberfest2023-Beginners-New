b4378c03318bd2c08fbd69f5afeeff33d3be44b6
Neha Kumari <111626458+SimplyReal@users.noreply.github.com>
2023-10-17 17:14:45 +0530

    Create hello_world_SimplyReal.cpp

---
+++ Scripts/hello_world_SimplyReal.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: Neha Kumari
+// GITHUB: https://github.com/SimplyReal
+
+# This program is in C++!
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, Hacktoberfest 2023 !";
+}
